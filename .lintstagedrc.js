507f19180880236b949d4b77f49ed9eb5c66d6a0
chetzof <vcosvic@gmail.com>
2022-12-28 15:06:23 +0200

    fix: all around improvements

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*': ['npm run fix:eslint --', 'npm run fix:prettier --'],
-  '**/*.ts?(x)': [() => 'npm run lint:tsc', 'vitest --changed'],
+  '**/*.ts?(x)': [() => 'npm run lint:tsc', 'vitest'],
 }
