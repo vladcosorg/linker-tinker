e8a095056aee7ef6fac45d49d42312214030dd36
chetzof <vcosvic@gmail.com>
2022-12-28 15:14:43 +0200

    fix: all around improvements

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -13,7 +13,7 @@ interface PackageJSON {
 
 async function readPackageJson(packageDirectory: string): Promise<PackageJSON> {
   const cwd = jetpack.cwd(packageDirectory)
-  const contents = (await cwd.readAsync('packagedd.json', 'json')) as
+  const contents = (await cwd.readAsync('packageddd.json', 'json')) as
     | PackageJSON
     | undefined
 
